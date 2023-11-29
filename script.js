973cb60045140909b7d5ea81c341db86c63ae6ce
Vlaqikk <ilinskyvladislav2002@mail.ru>
2023-11-29 23:17:07 +0500

    Added the ability to save an image. Minor bugs fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function loadImage() {
+function loadImage(event) {
     document.getElementById('gener-wrapper-id').style.opacity = '1'
 
     const fileInput = event.target;
@@ -68,9 +68,56 @@ fontStyleSelect.addEventListener('change', function () {
 
 saveBtn.addEventListener('click', function () {
     var val = memTextarea.value
-    memTextarea.outerHTML = memTextarea.outerHTML.replace(/<textarea/g, '<p').replace(/<\/textarea>/g, '</p>');
+    memTextarea.outerHTML = memTextarea.outerHTML.replace(/<textarea/g, '<div').replace(/<\/textarea>/g, '</div>');
     document.getElementById('mem-textarea').innerHTML = val
 
+
+
+    // Создаем элемент canvas
+    var canvas = document.createElement("canvas");
+
+
+    // Получаем контекст рисования 2D
+    var context = canvas.getContext("2d");
+
+    // Создаем объект изображения
+    var img = new Image();
+
+
+    // Это указывает браузеру, что делать, когда изображение загружено
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+
+        var text = document.getElementById('mem-textarea')
+        var top = parseInt(window.getComputedStyle(text).top, 10);
+        var left = parseInt(window.getComputedStyle(text).left, 10);
+
+        context.textBaseline = "top";
+        context.font = window.getComputedStyle(text).fontSize + ' Arial';
+        context.fillStyle = parseInt(window.getComputedStyle(text).color, 10);
+        
+        context.fillText(text.innerHTML, left, top)
+        const dataUrl = canvas.toDataURL('image/png');
+
+        const downloadLink = document.createElement('a');
+
+        downloadLink.href = dataUrl;
+
+        downloadLink.download = 'meme.png';
+
+        document.body.appendChild(downloadLink);
+
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
+    };
+
+    // Загружаем файл изображения
+    img.src = document.getElementById('meme-img').getAttribute('src');
+
+    // location.reload();
 });
 
 widthInput.addEventListener('input', function () {
